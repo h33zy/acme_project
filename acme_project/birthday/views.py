7a7eba7390ec22806cccdb8c48cc1bf3270d5c74
Никита Давыдкин <h33zy@yandex.ru>
2023-05-05 21:41:56 +0300

    end of validators

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,13 +1,17 @@
-# birthday/views.py 
-from django.shortcuts import render
+# birthday/views.py
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import BirthdayForm
 # Импортируем из utils.py функцию для подсчёта дней.
 from .utils import calculate_birthday_countdown
 from .models import Birthday
 
-def birthday(request):
-    form = BirthdayForm(request.POST or None)
+def birthday(request, pk=None):
+    if pk is not None:
+        instance = get_object_or_404(Birthday, pk=pk)
+    else:
+        instance = None
+    form = BirthdayForm(request.POST or None, instance=instance)
     # Создаём словарь контекста сразу после инициализации формы.
     context = {'form': form}
     # Если форма валидна...
@@ -28,3 +32,12 @@ def birthday_list(request):
     template_name = 'birthday/birthday_list.html'
     context = {'birthdays': birthdays}
     return render(request, template_name, context)
+
+def delete_birthday(request, pk):
+    instance = get_object_or_404(Birthday, pk=pk)
+    form = BirthdayForm(instance=instance)
+    context = {'form': form}
+    if request.method == 'POST':
+        instance.delete()
+        return redirect('birthday:list')
+    return render(request, 'birthday/birthday.html', context)
